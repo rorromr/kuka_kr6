5a4d071b1726506eb26b02b35b8d35cffa1f8513
Rodrigo Muñoz <rorro.mr@gmail.com>
2016-07-27 14:11:01 -0400

    Upd gripper

--- kuka_driver/scripts/gripper.py
+++ kuka_driver/scripts/gripper.py
@@ -1,34 +1,52 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Bool
+from threading import Thread
 
-class Led(object):
+
+class Gripper(object):
     def __init__(self, topic = '/gripper_cmd'):
         self.pub = rospy.Publisher('/gripper_cmd', Bool, queue_size=10)
         self.msg = Bool()
+        # Publish msg on thread
+        self.state_update_rate = 10
+        self.is_running = True
+        self.pub_thread = None
+
+    def start(self):
+        self.pub_thread = Thread(target=self.update_state)
+        self.pub_thread.start()
 
     def set(self, status = False):
         self.msg.data = status
-        self.pub.publish(self.msg)
-
-    def on(self):
+        
+    def close(self):
         self.set(True)
 
-    def off(self):
+    def open(self):
         self.set(False)
 
-        
+    def update_state(self):
+        rate = rospy.Rate(self.state_update_rate) 
+        while not rospy.is_shutdown() and self.is_running:  
+            self.pub.publish(self.msg)
+            rate.sleep()
+    
+    def shutdown(self):
+        self.is_running=False
+
 
 def main():  
     rospy.init_node('gripper_commander', anonymous=True)
     rate = rospy.Rate(1) # 1hz
-    led = Led()
-
+    gripper= Gripper()
+    gripper.start()
     while not rospy.is_shutdown():
-        led.on()
-        rate.sleep()
-        led.off()
-        rate.sleep()
+        gripper.close()
+        rospy.sleep(1.0)
+        gripper.open()
+        rospy.sleep(1.0)
+    gripper.shutdown()
 
 if __name__ == '__main__':
     try:
